fcf18fafd86b4f1cdd29525ecfe862463df62102
Enchanted-Millet <rocheers@hotmail.com>
2025-04-06 23:27:05 -0700

    feat: improve UX

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import Hero from '@/components/hero';
 import Link from 'next/link';
-// import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-// import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-// import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Home() {
   return (
@@ -10,14 +7,27 @@ export default async function Home() {
       <Hero />
       <main className="flex-1 flex flex-col px-4">
         <h2 className="font-medium text-xl mb-2">Next steps</h2>
-        <div className="flex gap-2">
-          Go to{' '}
-          <Link href="/leetcode" className="font-bold hover:underline">
-            Leetcode
-          </Link>{' '}
-          page to submit your session
-        </div>
-        {/* {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
+        <ol className="list-inside list-[upper-roman]">
+          <li>
+            <Link href="/sign-up" className="font-bold hover:underline">
+              Sign up
+            </Link>{' '}
+            first
+          </li>
+          <li>
+            <Link href="/sign-in" className="font-bold hover:underline">
+              Sign in
+            </Link>{' '}
+            your account
+          </li>
+          <li>
+            Go to{' '}
+            <Link href="/leetcode" className="font-bold hover:underline">
+              Leetcode
+            </Link>{' '}
+            page to submit your session
+          </li>
+        </ol>
       </main>
     </>
   );
